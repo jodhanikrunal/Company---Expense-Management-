46372d38145cf06df67889370b10a30303b87757
jodhanikrunal <21ce045@charusat.edu.in>
2023-08-13 21:10:57 +0530

    Add Schema for AddProject

---
+++ Comp_exp_mng_BackEnd/Models/Projects.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const ProjectSchme = mongoose.Schema({
+    companyEmail: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Register",
+        required: true,
+      },
+    projectTitle: {
+        type: String,
+        required: true,
+    },
+    projectDescription: {
+        type: String,
+        required: true,
+    },
+    maxBudget: {
+        type: Number,
+        required: true,
+    },
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    endDate: {
+        type: Date,
+        required: true,
+    },
+    projectManager: {
+        type: String,
+        required: true,
+    },
+    progress: {
+        type: Number,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+    },
+    projectMembers: [
+        {
+            employeeName: {
+                type: String,
+            },
+            employeeDepartment: {
+                type: String,
+            },
+            employeeDesignation: {
+                type: String,
+            },
+        }
+    ], 
+});
+
+const Project = mongoose.model("Project", ProjectSchme);
+module.exports = Project;
